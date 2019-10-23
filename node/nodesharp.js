6eb3eb0f1f98313f5664ffd4f779051e6a58201c
zhangyayun <zhangyy@greatld.com>
2019-10-23 19:52:17 +0800

    es6

--- node/nodesharp.js
+++ node/nodesharp.js
@@ -2,6 +2,7 @@
 const child_process = require("child_process")
 const iconv = require('iconv-lite');
 const extend = require('util')._extend;
+const EventEmitter = require('events').EventEmitter;
 const path = require('path');
 
 let S4 = () => {
@@ -12,146 +13,163 @@ let guid = () => {
     return (S4() + S4() + S4() + S4() + S4() + S4() + S4() + S4());
 };
 
-let NodeSharp = function (option, messageCallback, errorCallback) {
-    this.childprocess = null;
-    this.onceListenters = {};
-    this.alawysListenters = {};
-    this.cachedata = "";
-    this.alwaysListenter = null;
-    this.errorListenter = null;
-    this.trueClose = false;
-    let me = this;
-
-    let defaultOpt = {
-        exe: 'NYSProcess.exe',
-        closeCallback: function (exitcode) {},
-        autoReOpen: false,
-        args: []
-    };
-
-    let opt = null;
-    if (typeof option == 'string') {
-        defaultOpt.exe = option;
-        opt = defaultOpt;
-    } else {
-        opt = extend(defaultOpt, option);
+let getparam = (obj) => {
+    return {
+        cguid: guid(),
+        data: obj
     }
-    opt.args = ['pos'].concat(opt.args);
+};
 
+/**
+ * 
+ */
+class NodeSharp extends EventEmitter {
+    constructor(option) {
+        super();
+        this.childprocess = null;
+        this.onceListenters = {};
+        this.alawysListenters = {};
+        this.cachedata = "";
+        this._needReOpen = false;
+        this.opt = {};
 
-    let getparam = function (obj) {
-        return {
-            cguid: guid(),
-            data: obj
+        let defaultOpt = {
+            exePath: '',
+            autoReOpen: false,
+            args: [],
+            dataListener: null,
+            errorListener: null
+        };
+
+        if (typeof option == 'string') {
+            defaultOpt.exePath = option;
+            this.opt = defaultOpt;
+        } else {
+            this.opt = extend(defaultOpt, option);
         }
-    };
+        // this.opt.args = ['pos'].concat(this.opt.args);
+        if (this.opt.dataListener) {
+            this.on('data', dataListener)
+        }
+        if (this.opt.errorListener) {
+            this.on('error', errorListener)
+        }
+    }
 
-    this.open = function () {
+    /**
+     * start a exe and connect
+     * @param {*} option exePath OR {exePath，autoReOpen，args, dataListener, errorListener }
+     */
+    static connect(option) {
+        let exe = new this(option)
+        exe.open();
+        return exe;
+    }
+
+
+
+    open() {
         let exec = child_process.spawn;
-        let exepath = path.resolve(__dirname, opt.exe);
-        me.childprocess = exec(exepath, opt.args);
-        me.childprocess.stdin.setEncoding("binary");
-        me.childprocess.stdout.setEncoding("binary");
-        if (me.childprocess == null && opt.closeCallback)
-            opt.closeCallback(-1);
-        me.childprocess.on('exit', function (code) {
-            if (opt.autoReOpen && code != 0 && !me.trueClose)
+        let exepath = path.resolve(__dirname, this.opt.exePath);
+        this.childprocess = exec(exepath, this.opt.args);
+        this.childprocess.stdin.setEncoding("binary");
+        this.childprocess.stdout.setEncoding("binary");
+        if (this.childprocess == null && this.opt.closeCallback)
+            this.opt.closeCallback(-1);
+        this.childprocess.on('exit', (code, signal) => {
+            if ((this.opt.autoReOpen && signal !== 'SIGTERM') || this._needReOpen)
                 setTimeout(function () {
-                    me.open();
-                    console.log('reopen' + opt.exe, !!me.childprocess);
-                }, 2000);
-            if (opt.closeCallback)
-                opt.closeCallback(code);
+                    this.open();
+                    this.emit('reopen', this);
+                }.bind(this), 1000);
+            this._needReOpen = false;
+            this.emit('close', code, this.trueClose);
         });
 
-        me.childprocess.stdout.on('data', function (data) {
+        this.childprocess.stdout.on('data', (data) => {
+            this.emit('message', data);
             let json = '';
             try {
                 json = data.toString();
                 if (json.endsWith('\r\n'))
                     json = json.substr(0, json.length - 1);
-                json = me.cachedata + json;
-                me.cachedata = "";
-                var resultdatas = json.split('@=@');
-                for (var j in resultdatas) {
+                json = this.cachedata + json;
+                this.cachedata = "";
+                let resultdatas = json.split('@=@');
+                for (let j in resultdatas) {
                     if (resultdatas.length == 1) {
-                        me.cachedata += resultdatas[j];
+                        this.cachedata += resultdatas[j];
                         continue;
                     }
                     if (j == (resultdatas.length - 1) && resultdatas[j] != '') {
-                        me.cachedata += resultdatas[j];
+                        this.cachedata += resultdatas[j];
                         continue;
                     }
-
-                    var jsondata = "";
+                    let jsondata = "";
                     try {
-                        var jsondata = JSON.parse(resultdatas[j]);
+                        jsondata = JSON.parse(resultdatas[j]);
                     } catch (ee) {
-                        me.cachedata += resultdatas[j];
-                        //console.error(resultdatas[j]);
+                        this.cachedata += resultdatas[j];
                         continue;
                     }
-                    if (me.onceListenters[jsondata.cguid]) {
-                        me.onceListenters[jsondata.cguid](jsondata.Error, jsondata.data);
-                        delete me.onceListenters[jsondata.cguid];
+                    if (this.onceListenters[jsondata.cguid]) {
+                        this.onceListenters[jsondata.cguid](jsondata.Error, jsondata.data);
+                        delete this.onceListenters[jsondata.cguid];
                     }
-
-                    if (me.alawysListenters[jsondata.cguid]) {
-                        me.alawysListenters[jsondata.cguid](jsondata.Error, jsondata.data);
+                    if (this.alawysListenters[jsondata.cguid]) {
+                        this.alawysListenters[jsondata.cguid](jsondata.Error, jsondata.data);
                     }
-
-                    if (me.alwaysListenter)
-                        me.alwaysListenter(jsondata.data, jsondata.Error);
-                    if (me.errorListenter && jsondata.Error && jsondata.Error.length > 2)
-                        me.errorListenter(jsondata.Error);
+                    this.emit('data', jsondata.Error, jsondata.data);
                 }
             } catch (e) {
-                if (me.errorListenter)
-                    me.errorListenter(e.message);
-                console.error(e);
-                //console.error(json);
+                this.emit('error', e);
             }
         });
-        me.childprocess.stdout.on('error', e => {
+        this.childprocess.stdout.on('error', e => {
             console.error(e)
-            errorCallback(e)
+            this.emit('error', e)
         })
-        me.childprocess.stdin.on('error', e => {
+        this.childprocess.stdin.on('error', e => {
             console.error(e)
-            errorCallback(e)
+            this.emit('error', e)
         })
     };
 
-    this.close = function (close) {
-        me.trueClose = !!close;
-        me.childprocess.kill();
+    close() {
+        this._needReOpen = false;
+        if (!this.childprocess.killed)
+            this.childprocess.kill('SIGTERM');
     };
 
-    this.send = function ({
+    reOpen() {
+        this._needReOpen = true;
+        if (!this.childprocess.killed)
+            return this.childprocess.kill();
+        this.open();
+    }
+
+    send({
         param,
         oncecb,
         alawyscb
     }) {
-        if (!me.childprocess && oncecb)
-            return oncecb('所需进程尚未启动');
-        if (!me.childprocess.stdin.writable && oncecb)
-            return oncecb('无法建立通道连接');
-        if (!me.childprocess && alawyscb)
-            return alawyscb('所需进程尚未启动');
-        if (!me.childprocess.stdin.writable && alawyscb)
-            return alawyscb('无法建立通道连接');
+        if (!this.childprocess && oncecb)
+            return oncecb('The required process has not started');
+        if (!this.childprocess.stdin.writable && oncecb)
+            return oncecb('Unable to establish channel connection');
+        if (!this.childprocess && alawyscb)
+            return alawyscb('The required process has not started');
+        if (!this.childprocess.stdin.writable && alawyscb)
+            return alawyscb('Unable to establish channel connection');
         param = getparam(param);
         //console.log(JSON.stringify(data));
         if (oncecb)
-            me.onceListenters[param.cguid] = oncecb;
+            this.onceListenters[param.cguid] = oncecb;
         if (alawyscb)
-            me.alawysListenters[param.cguid] = alawyscb;
+            this.alawysListenters[param.cguid] = alawyscb;
         let senddata = iconv.encode(JSON.stringify(param) + "\r\n", 'gbk');
-        me.childprocess.stdin.write(senddata);
+        this.childprocess.stdin.write(senddata);
     };
+}
 
-    me.alwaysListenter = messageCallback;
-    me.errorListenter = errorCallback;
-    me.open();
-};
 module.exports = NodeSharp;
